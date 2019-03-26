111f869d58af033f82828856e46200faa6240722
JS <catherlove@163.com>
2019-03-26 10:06:30 +0800

    项目初建

---
+++ src/main/java/com/ajs/Db2excelApplication.java
@@ -0,0 +1,13 @@
+package com.ajs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Db2excelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Db2excelApplication.class, args);
+    }
+
+}
