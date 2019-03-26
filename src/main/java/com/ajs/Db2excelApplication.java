19091e03a8e2e4f470ac553c9af23312d57e587e
JS <catherlove@163.com>
2019-03-26 11:04:56 +0800

    项目生成

--- src/main/java/com/ajs/Db2excelApplication.java
+++ src/main/java/com/ajs/Db2excelApplication.java
@@ -1,13 +1,26 @@
 package com.ajs;
 
+import com.ajs.thread.BookThread;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.Resource;
+
 @SpringBootApplication
-public class Db2excelApplication {
+public class Db2excelApplication implements CommandLineRunner {
+
+    @Resource
+    private BookThread bookThread;
 
     public static void main(String[] args) {
         SpringApplication.run(Db2excelApplication.class, args);
     }
 
+    @Override
+    public void run(String... strings) throws Exception {
+
+        Thread thBookThread = new Thread(bookThread);
+        thBookThread.start();
+    }
 }
